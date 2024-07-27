0969ee37b29d8829f5f14e6fc4ad13411fb68009
Ricky Yang <rickyxiaoyang@gmail.com>
2024-07-27 08:37:35 -0400

    fix: title

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <a href="/">
       <header className="text-center">
-        <div className="text-2xl font-bold capitalize">ROAD TO YUMMY</div>
+        <div className="text-2xl font-bold uppercase leading-5">
+          ROAD TO YUMMY
+        </div>
         <div className="text-sm font-thin">from hungry to happy</div>
       </header>
     </a>
