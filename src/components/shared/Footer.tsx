921f103ee9b7ec8a837d956970a30fa4c67b3819
Ricky Yang <rickyxiaoyang@gmail.com>
2024-08-04 16:55:49 -0400

    fix: add padding

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-center">
+    <div className="text-center py-4">
       <div className="text-xs font-thin">ROAD TO YUMMY</div>
       <div className="text-xs font-thin">by Amy & Ricky</div>
     </div>
